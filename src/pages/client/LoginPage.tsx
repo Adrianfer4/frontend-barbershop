545630b155b8d0dd4a2d210e084f1e636c3d846f
Adrianfer4 <yerssondereck@gmail.com>
2026-02-03 11:10:07 -0500

    Correccion de diseño responsivo de login y registro

--- src/pages/client/LoginPage.tsx
+++ src/pages/client/LoginPage.tsx
@@ -6,7 +6,7 @@ export default function LoginPage() {
       className="min-h-screen flex items-center justify-center bg-cover bg-center px-4 py-8"
       style={{ backgroundImage: "url('/brick-wall.jpg')" }}
     >
-      <div className="bg-white bg-opacity-90 rounded-3xl shadow-3xl flex flex-col-reverse md:flex-row w-full max-w-sm md:max-w-none md:w-[85%] lg:w-[50%] xl:w-[40%]">
+      <div className="bg-white bg-opacity-90 rounded-3xl shadow-3xl flex flex-col-reverse md:flex-row w-full md:max-w-none md:w-[85%] lg:w-[50%] xl:w-[40%]">
         <LoginForm />
         <SideImage imageUrl="/barberia-login.jpg" />
       </div>
