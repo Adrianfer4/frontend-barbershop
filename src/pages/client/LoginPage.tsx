7dadba31bb3736062b4aa7090a1f12959d46144d
Adrianfer4 <yerssondereck@gmail.com>
2026-02-03 10:44:05 -0500

    Fix de diseño responsivo de login y register

--- src/pages/client/LoginPage.tsx
+++ src/pages/client/LoginPage.tsx
@@ -3,10 +3,10 @@ import SideImage from "../../components/autentication/ImageCard";
 export default function LoginPage() {
   return (
     <div
-      className="min-h-screen flex items-center justify-center bg-cover bg-center px-4"
+      className="min-h-screen flex items-center justify-center bg-cover bg-center px-4 py-8"
       style={{ backgroundImage: "url('/brick-wall.jpg')" }}
     >
-      <div className="bg-white bg-opacity-90 rounded-3xl shadow-3xl flex flex-col-reverse md:flex-row w-full md:w-[85%] lg:w-[50%] xl:w-[40%] max-w-4xl">
+      <div className="bg-white bg-opacity-90 rounded-3xl shadow-3xl flex flex-col-reverse md:flex-row w-full max-w-sm md:max-w-none md:w-[85%] lg:w-[50%] xl:w-[40%]">
         <LoginForm />
         <SideImage imageUrl="/barberia-login.jpg" />
       </div>
